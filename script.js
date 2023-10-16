c9573976e1fd7bfa19b52adfbd00289e6a34eff6
maurasal <144288977+maurasal@users.noreply.github.com>
2023-10-16 18:59:02 -0500

    updated the questions on the quiz, added a form submission for logging scores

--- script.js
+++ script.js
@@ -1,30 +1,94 @@
-var timerEl = document.getElementById('timer')
+var timerEl = document.getElementById('timer');
+const startButton = document.getElementById('startButton');
+const questionEl = document.getElementById("question");
+const optionsEl = document.getElementById("options");
+//moved timer variables OUTSIDE of the startQuiz function so it's globally available
+let timeD;
+let timeRemaining = 30;
 
-const startButton = document.getElementById('startButton')
+startButton.addEventListener('click', startQuiz);
 
-startButton.addEventListener('click', countdown)
-startButton.addEventListener('click', askQuestion)
+const quizQuestions = [
+  {
+    question: "What color is a 'bay' horse?",
+    options: ["White", "Brown", "Black"],
+    correctAnswer: 1
+  },
+  {
+    question: "What is the name of the piece of equipment that goes over a horse's head?",
+    options: ["Saddle", "Martingale", "Bridle"],
+    correctAnswer: 2
+  },
+  {
+    question: "What unit of measurment is used to determine a horse's height measured?",
+    options: ["Feet", "Inches", "Hands"],
+    correctAnswer: 2
+  },
+  {
+    question: "What is the profession of someone who makes horse shoes?",
+    options: ["Farrier", "Veterinarian", "Doctor"],
+    correctAnswer: 0
+  },
+];
 
-function countdown() {
-    var timeLeft = 30;
-  
-    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-    var timeInterval = setInterval(function () {
-      if (timeLeft > 1) {
-        timerEl.textContent = timeLeft + ' seconds remaining';
-        timeLeft--;
-      } else if (timeLeft === 1) {
-        timerEl.textContent = timeLeft + ' second remaining';
-        timeLeft--;
-      } else {
-        timerEl.textContent = '';
-        clearInterval(timeInterval);
-        // Call the `displayMessage()` function
-        displayMessage();
-      }
-    }, 1000);
+let currentQuestion = 0;
+let score = 0;
+let timer;
+
+function displayQuestion() {
+
+  questionEl.textContent = quizQuestions[currentQuestion].question;
+
+  optionsEl.innerHTML = "";
+  quizQuestions[currentQuestion].options.forEach((option, index) => {
+    const optionButton = document.createElement("button");
+    optionButton.textContent = option;
+    optionButton.addEventListener("click", () => {
+      checkAnswer(index);
+    });
+    optionsEl.appendChild(optionButton);
+  });
+}
+
+function checkAnswer(selectedIndex) {
+  if (selectedIndex === quizQuestions[currentQuestion].correctAnswer) {
+    score++;
+  } else {
+    timeRemaining -= 10
   }
+  currentQuestion++;
+  if (currentQuestion === quizQuestions.length) {
+    endQuiz();
+  } else {
+    displayQuestion();
+  }
+}
 
-function askQuestion() {
+function endQuiz() {
+  clearInterval(timeD);
+  console.log("Quiz ended. Your score: " + score);
+  console.log('Time is up!');
+  questionElement.textContent = ("Enter your initials to record your score")
+}
 
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+ 
+    var inputValue = document.getElementById("textInput").value;
+
+    console.log("Saved Scores: " + inputValue + " had a score of " + score);
+    document.getElementById("textInput").value = "";
+});
+
+function startQuiz() {
+  const countdownDisplay = document.getElementById('timer');
+  timeD = setInterval(() => {
+    timeRemaining--;
+    countdownDisplay.textContent = ("You have " + timeRemaining + " second(s) left");
+    if (timeRemaining === 0) {
+      endQuiz()
+    }
+  }, 1000);
+
+  displayQuestion();
 }
